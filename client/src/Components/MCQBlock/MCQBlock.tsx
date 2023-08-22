0eec40e247ac1cd8470c1c32aa2acd2ba343cb26
isabelmavery <isabelmavery@gmail.com>
2023-08-22 11:31:57 -0400

    feat: shuffle options in mqc

--- client/src/Components/MCQBlock/MCQBlock.tsx
+++ client/src/Components/MCQBlock/MCQBlock.tsx
@@ -1,10 +1,27 @@
-import { useState } from 'react'
+import { useMemo, useState } from 'react'
 import { RawBlock } from '../../types/block.types'
 import '../../App.css'
 import './MCQBlock.css'
 
 const FEEDBACK_INCORRECT_PREFIX = 'Incorrect - '
 const FEEDBACK_CORRECT_TEXT = 'Correct - Nice work!'
+
+/**
+ * Adds to correct option into the options array at a random location
+ * So the correct option is not always in the same place.
+ */
+function shuffleCorrectOption(
+  IncorrectOptions: string[],
+  CorrectOption?: string
+) {
+  if (!CorrectOption) return IncorrectOptions
+  const randomizedIndex = Math.floor(Math.random() * IncorrectOptions.length)
+  return [
+    ...IncorrectOptions.slice(0, randomizedIndex),
+    CorrectOption,
+    ...IncorrectOptions.slice(randomizedIndex),
+  ]
+}
 
 function Option({
   optionName,
@@ -25,22 +42,6 @@ function Option({
   )
 }
 
-/**
- * Adds to correct option into the options array at a random location
- * So the correct option is not always in the same place.
- */
-// function shuffleCorrectOption(
-//   IncorrectOptions: string[],
-//   CorrectOption: string
-// ) {
-//   const randomizedIndex = Math.floor(Math.random() * IncorrectOptions.length)
-//   return [
-//     ...IncorrectOptions.slice(0, randomizedIndex),
-//     CorrectOption,
-//     ...IncorrectOptions.slice(randomizedIndex),
-//   ]
-// }
-
 export default function MCQBlock(props: {
   block: RawBlock
   blockIndex: number
@@ -50,18 +51,21 @@ export default function MCQBlock(props: {
       Object: {
         Question,
         CorrectOption,
-        IncorrectOptions,
+        IncorrectOptions = [],
         FeedbackOnIncorrect,
       },
     },
     blockIndex,
   } = props
+  const [selectedOption, setSelectedOption] = useState<string | null>(null)
+  const allOptions = useMemo(
+    () => shuffleCorrectOption(IncorrectOptions, CorrectOption),
+    [IncorrectOptions, CorrectOption]
+  )
   const quizName = `quiz-q-${blockIndex}`
 
-  const [selectedOption, setSelectedOption] = useState<string | null>(null)
-
   function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
-    // Validate
+    // Validate that the selected option is correct
     if (selectedOption !== CorrectOption) {
       alert(`${FEEDBACK_INCORRECT_PREFIX}${FeedbackOnIncorrect}`)
     } else {
@@ -80,7 +84,7 @@ export default function MCQBlock(props: {
       <form name={quizName} onSubmit={handleSubmit}>
         <div style={{ marginBottom: 10 }}>{Question}</div>
 
-        {[...IncorrectOptions, CorrectOption].map((o: string) => (
+        {allOptions.map((o: string) => (
           <Option
             quizName={quizName}
             optionName={o}
